b73691beedb1ee1aa0178394c9d8a58c968e422f
SaraAnn Stanway <sarastanway@gmail.com>
2016-04-17 04:51:43 -0400

    fixed history query, hasSinned

--- tap_chrome_files/eventPage.js
+++ tap_chrome_files/eventPage.js
@@ -1,22 +1,29 @@
-      console.log("time",time);
-      console.log("phoneNumber", phoneNumber);
+
+      console.log("hey dumbass");
+      var hasSinned = false;
       var date = new Date();
       var alarmStartTime = date.getTime();
+      var facebook = /facebook/gi;
+      var youtube = /youtube/gi;
+      var twitter = /twitter/gi;
+      var forbidden = [facebook, youtube, twitter];
       chrome.alarms.create("alarm",{delayInMinutes:1 } );
 
       chrome.alarms.onAlarm.addListener(function ()
       {
+          var endDate = new Date();
           //background listener
-          chrome.history.search({text:"", startTime:alarmStartTime, endTime:date.getTime()}, function(data)
+          chrome.history.search({text:"", startTime:alarmStartTime, endTime:endDate.getTime()}, function(data)
      		{
-     			data.forEach(function(page)
-     			{
-     				console.log(page.URL);
-            
-              alert("you searched google");
-            
-              
-     			});
+     			data.forEach(function(page){
+           console.log(page.url);
+            if (page.url.search(facebook)!=-1||page.url.search(youtube)!=-1||page.url.search(twitter)!=-1)
+            {
+              hasSinned = true;
+            }
+            console.log(hasSinned);
+
+          });
      		});
           console.log("ayo");
-       });
+       })
--- tap_chrome_files/popup.js
+++ tap_chrome_files/popup.js
@@ -1,51 +1,13 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
   console.log("hi");
   var checkPageButton = document.getElementById('buttonclick');
   checkPageButton.addEventListener('click', function() {
-console.log("clicked");
+  console.log("clicked");
 
       d = document;
-      var phoneNumber = "+1" + d.getElementById("phone_value").value;
-      var time = Number(d.getElementById("timer_value").value);
-
-      console.log("time",time);
-      console.log("phoneNumber", phoneNumber);
-<<<<<<< HEAD
-      var date = new Date();
-      var alarmStartTime = date.getTime();
-      chrome.alarms.create("alarm",{delayInMinutes:1 } );
-
-      chrome.alarms.onAlarm.addListener(function ()
-      {
-          //background listener
-          chrome.history.search({text:"", startTime:alarmStartTime, endTime:date.getTime()}, function(data)
-     		{
-     			data.forEach(function(page)
-     			{
-     				console.log(page.url);
-     			});
-     		});
-          console.log("ayo");
-       });
-=======
-      chrome.alarms.create("alarm",{delayInMinutes:time } );
-
-
-      chrome.alarms.onAlarm.addListener(function (){
-
-          //background listener
-          
-
-
-          console.log("hey it works!");
-        });
-
-
->>>>>>> 3008e26e77011c69f690a83d62b9abb7ca56eb65
-
+      var phoneNumber = d.getElementById("phone_value").value;
+      var time = d.getElementById("timer_value").value;
+      chrome.runtime.getBackgroundPage;
 
   }, false);
 }, false);
